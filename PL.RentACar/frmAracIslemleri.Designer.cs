056e4332b144f126e5ba14b352f1aebff4634ebe
MertOzek <720694-Ogle@YASIN-PC>
2019-01-23 20:37:26 +0300

    değişik.

--- PL.RentACar/frmAracIslemleri.Designer.cs
+++ PL.RentACar/frmAracIslemleri.Designer.cs
@@ -28,10 +28,17 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.SuspendLayout();
+            // 
+            // frmAracIslemleri
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "frmAracIslemleri";
+            this.Name = "frmAracIslemleri";
+            this.Text = "frmAracIslemlerii";
+            this.ResumeLayout(false);
+
         }
 
         #endregion
