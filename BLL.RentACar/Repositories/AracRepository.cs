c745443aadd9d2d0b90f360a2120dd9353c30d2c
720694-Ogle <720694-Ogle@WISSEN>
2019-01-24 20:03:35 +0300

    İmplemenler yapıldı.

---
+++ BLL.RentACar/Repositories/AracRepository.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DAL.RentACar.Context;
+
+namespace BLL.RentACar.Repositories
+{
+    public class AracRepository : IAracRepository
+    {
+        public bool AracEkle(Arac A)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<Arac> AracGetir()
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<Arac> AracGetirById(int ID)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<Arac> AracGetirByPlaka(string Plaka)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool AracGuncelle(Arac A)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool AracSil(int ID)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool AracSil(Arac A)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
