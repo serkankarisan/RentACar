7649fa87557b9b5323462b181573dc537c9f12bd
denemem <720694-Ogle@DESKTOP-3HSG3TM>
2019-01-25 17:24:21 +0300

    Araç Sorgulama Sayfasına ListView eklendi

--- PL.RentACar/frmAracSorgulama.Designer.cs
+++ PL.RentACar/frmAracSorgulama.Designer.cs
@@ -32,34 +32,41 @@
             this.label1 = new System.Windows.Forms.Label();
             this.txtAracModel = new System.Windows.Forms.TextBox();
             this.txtPlaka = new System.Windows.Forms.TextBox();
-            this.dataGridView1 = new System.Windows.Forms.DataGridView();
-            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.label3 = new System.Windows.Forms.Label();
+            this.lvDetaylar = new System.Windows.Forms.ListView();
+            this.columnHeader6 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader1 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader2 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader3 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader4 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader5 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.cbMarkalar = new System.Windows.Forms.ComboBox();
             this.SuspendLayout();
             // 
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
-            this.label2.Location = new System.Drawing.Point(364, 322);
+            this.label2.Font = new System.Drawing.Font("Lucida Bright", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(530, 18);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(89, 20);
+            this.label2.Size = new System.Drawing.Size(100, 18);
             this.label2.TabIndex = 9;
             this.label2.Text = "Arac Model";
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
-            this.label1.Location = new System.Drawing.Point(113, 322);
+            this.label1.Font = new System.Drawing.Font("Lucida Bright", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(279, 18);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(109, 20);
+            this.label1.Size = new System.Drawing.Size(121, 18);
             this.label1.TabIndex = 8;
             this.label1.Text = "Arac Plaka No";
             // 
             // txtAracModel
             // 
             this.txtAracModel.BackColor = System.Drawing.SystemColors.HighlightText;
-            this.txtAracModel.Location = new System.Drawing.Point(479, 322);
+            this.txtAracModel.Location = new System.Drawing.Point(645, 18);
             this.txtAracModel.Name = "txtAracModel";
             this.txtAracModel.Size = new System.Drawing.Size(100, 20);
             this.txtAracModel.TabIndex = 7;
@@ -67,33 +74,95 @@
             // txtPlaka
             // 
             this.txtPlaka.BackColor = System.Drawing.SystemColors.HighlightText;
-            this.txtPlaka.Location = new System.Drawing.Point(228, 322);
+            this.txtPlaka.Location = new System.Drawing.Point(406, 17);
             this.txtPlaka.Name = "txtPlaka";
             this.txtPlaka.Size = new System.Drawing.Size(100, 20);
             this.txtPlaka.TabIndex = 6;
             // 
-            // dataGridView1
+            // label3
             // 
-            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dataGridView1.Location = new System.Drawing.Point(98, 61);
-            this.dataGridView1.Name = "dataGridView1";
-            this.dataGridView1.Size = new System.Drawing.Size(527, 216);
-            this.dataGridView1.TabIndex = 5;
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Lucida Bright", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(13, 20);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(67, 18);
+            this.label3.TabIndex = 12;
+            this.label3.Text = "Marka :";
+            // 
+            // lvDetaylar
+            // 
+            this.lvDetaylar.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.columnHeader6,
+            this.columnHeader1,
+            this.columnHeader2,
+            this.columnHeader3,
+            this.columnHeader4,
+            this.columnHeader5});
+            this.lvDetaylar.Location = new System.Drawing.Point(12, 50);
+            this.lvDetaylar.Name = "lvDetaylar";
+            this.lvDetaylar.Size = new System.Drawing.Size(757, 266);
+            this.lvDetaylar.TabIndex = 11;
+            this.lvDetaylar.UseCompatibleStateImageBehavior = false;
+            this.lvDetaylar.View = System.Windows.Forms.View.Details;
+            // 
+            // columnHeader6
+            // 
+            this.columnHeader6.Text = "Model";
+            this.columnHeader6.Width = 100;
+            // 
+            // columnHeader1
+            // 
+            this.columnHeader1.Text = "Tip";
+            this.columnHeader1.Width = 100;
+            // 
+            // columnHeader2
+            // 
+            this.columnHeader2.Text = "Renk";
+            this.columnHeader2.Width = 100;
+            // 
+            // columnHeader3
+            // 
+            this.columnHeader3.Text = "Plaka";
+            this.columnHeader3.Width = 100;
+            // 
+            // columnHeader4
+            // 
+            this.columnHeader4.Text = "Durumu";
+            this.columnHeader4.Width = 100;
+            // 
+            // columnHeader5
+            // 
+            this.columnHeader5.Text = "Günlük Fiyat";
+            this.columnHeader5.Width = 136;
+            // 
+            // cbMarkalar
+            // 
+            this.cbMarkalar.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.cbMarkalar.FormattingEnabled = true;
+            this.cbMarkalar.Items.AddRange(new object[] {
+            "Audi",
+            "Renault",
+            "Toyota"});
+            this.cbMarkalar.Location = new System.Drawing.Point(102, 16);
+            this.cbMarkalar.Name = "cbMarkalar";
+            this.cbMarkalar.Size = new System.Drawing.Size(153, 21);
+            this.cbMarkalar.TabIndex = 10;
             // 
             // frmAracSorgulama
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.lvDetaylar);
+            this.Controls.Add(this.cbMarkalar);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.txtAracModel);
             this.Controls.Add(this.txtPlaka);
-            this.Controls.Add(this.dataGridView1);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
             this.Name = "frmAracSorgulama";
             this.Text = "frmAracSorgulama";
-            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -105,6 +174,14 @@
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.TextBox txtAracModel;
         private System.Windows.Forms.TextBox txtPlaka;
-        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.ListView lvDetaylar;
+        private System.Windows.Forms.ColumnHeader columnHeader6;
+        private System.Windows.Forms.ColumnHeader columnHeader1;
+        private System.Windows.Forms.ColumnHeader columnHeader2;
+        private System.Windows.Forms.ColumnHeader columnHeader3;
+        private System.Windows.Forms.ColumnHeader columnHeader4;
+        private System.Windows.Forms.ColumnHeader columnHeader5;
+        private System.Windows.Forms.ComboBox cbMarkalar;
     }
 }
