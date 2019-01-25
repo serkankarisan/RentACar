29a5c31a832f917fcab7973143d83cc5dc36bd63
denemem <720694-Ogle@DESKTOP-3HSG3TM>
2019-01-25 17:27:01 +0300

    aynı sayfa tekrar güncellendi.

--- PL.RentACar/frmAracSorgulama.Designer.cs
+++ PL.RentACar/frmAracSorgulama.Designer.cs
@@ -28,6 +28,8 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(frmAracSorgulama));
             this.label2 = new System.Windows.Forms.Label();
             this.label1 = new System.Windows.Forms.Label();
             this.txtAracModel = new System.Windows.Forms.TextBox();
@@ -41,13 +43,22 @@
             this.columnHeader4 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
             this.columnHeader5 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
             this.cbMarkalar = new System.Windows.Forms.ComboBox();
+            this.ilSmall = new System.Windows.Forms.ImageList(this.components);
+            this.ilLarge = new System.Windows.Forms.ImageList(this.components);
+            this.cmView = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.mitmLargeIcon = new System.Windows.Forms.ToolStripMenuItem();
+            this.mitmDetails = new System.Windows.Forms.ToolStripMenuItem();
+            this.mitmSmallIcon = new System.Windows.Forms.ToolStripMenuItem();
+            this.mitmList = new System.Windows.Forms.ToolStripMenuItem();
+            this.mitmTile = new System.Windows.Forms.ToolStripMenuItem();
+            this.cmView.SuspendLayout();
             this.SuspendLayout();
             // 
             // label2
             // 
             this.label2.AutoSize = true;
             this.label2.Font = new System.Drawing.Font("Lucida Bright", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label2.Location = new System.Drawing.Point(530, 18);
+            this.label2.Location = new System.Drawing.Point(530, 80);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(100, 18);
             this.label2.TabIndex = 9;
@@ -57,7 +68,7 @@
             // 
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Lucida Bright", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.Location = new System.Drawing.Point(279, 18);
+            this.label1.Location = new System.Drawing.Point(279, 80);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(121, 18);
             this.label1.TabIndex = 8;
@@ -66,7 +77,7 @@
             // txtAracModel
             // 
             this.txtAracModel.BackColor = System.Drawing.SystemColors.HighlightText;
-            this.txtAracModel.Location = new System.Drawing.Point(645, 18);
+            this.txtAracModel.Location = new System.Drawing.Point(645, 80);
             this.txtAracModel.Name = "txtAracModel";
             this.txtAracModel.Size = new System.Drawing.Size(100, 20);
             this.txtAracModel.TabIndex = 7;
@@ -74,7 +85,7 @@
             // txtPlaka
             // 
             this.txtPlaka.BackColor = System.Drawing.SystemColors.HighlightText;
-            this.txtPlaka.Location = new System.Drawing.Point(406, 17);
+            this.txtPlaka.Location = new System.Drawing.Point(406, 79);
             this.txtPlaka.Name = "txtPlaka";
             this.txtPlaka.Size = new System.Drawing.Size(100, 20);
             this.txtPlaka.TabIndex = 6;
@@ -83,7 +94,7 @@
             // 
             this.label3.AutoSize = true;
             this.label3.Font = new System.Drawing.Font("Lucida Bright", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label3.Location = new System.Drawing.Point(13, 20);
+            this.label3.Location = new System.Drawing.Point(13, 82);
             this.label3.Name = "label3";
             this.label3.Size = new System.Drawing.Size(67, 18);
             this.label3.TabIndex = 12;
@@ -98,9 +109,12 @@
             this.columnHeader3,
             this.columnHeader4,
             this.columnHeader5});
-            this.lvDetaylar.Location = new System.Drawing.Point(12, 50);
+            this.lvDetaylar.ContextMenuStrip = this.cmView;
+            this.lvDetaylar.LargeImageList = this.ilLarge;
+            this.lvDetaylar.Location = new System.Drawing.Point(12, 112);
             this.lvDetaylar.Name = "lvDetaylar";
             this.lvDetaylar.Size = new System.Drawing.Size(757, 266);
+            this.lvDetaylar.SmallImageList = this.ilSmall;
             this.lvDetaylar.TabIndex = 11;
             this.lvDetaylar.UseCompatibleStateImageBehavior = false;
             this.lvDetaylar.View = System.Windows.Forms.View.Details;
@@ -143,10 +157,67 @@
             "Audi",
             "Renault",
             "Toyota"});
-            this.cbMarkalar.Location = new System.Drawing.Point(102, 16);
+            this.cbMarkalar.Location = new System.Drawing.Point(102, 78);
             this.cbMarkalar.Name = "cbMarkalar";
             this.cbMarkalar.Size = new System.Drawing.Size(153, 21);
             this.cbMarkalar.TabIndex = 10;
+            // 
+            // ilSmall
+            // 
+            this.ilSmall.ImageStream = ((System.Windows.Forms.ImageListStreamer)(resources.GetObject("ilSmall.ImageStream")));
+            this.ilSmall.TransparentColor = System.Drawing.Color.Transparent;
+            this.ilSmall.Images.SetKeyName(0, "q7.jpg");
+            this.ilSmall.Images.SetKeyName(1, "a7.jpg");
+            this.ilSmall.Images.SetKeyName(2, "a3hatchback.jpg");
+            // 
+            // ilLarge
+            // 
+            this.ilLarge.ImageStream = ((System.Windows.Forms.ImageListStreamer)(resources.GetObject("ilLarge.ImageStream")));
+            this.ilLarge.TransparentColor = System.Drawing.Color.Transparent;
+            this.ilLarge.Images.SetKeyName(0, "q7.jpg");
+            this.ilLarge.Images.SetKeyName(1, "a7.jpg");
+            this.ilLarge.Images.SetKeyName(2, "a3hatchback.jpg");
+            // 
+            // cmView
+            // 
+            this.cmView.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.mitmLargeIcon,
+            this.mitmDetails,
+            this.mitmSmallIcon,
+            this.mitmList,
+            this.mitmTile});
+            this.cmView.Name = "cmView";
+            this.cmView.Size = new System.Drawing.Size(181, 136);
+            // 
+            // mitmLargeIcon
+            // 
+            this.mitmLargeIcon.Name = "mitmLargeIcon";
+            this.mitmLargeIcon.Size = new System.Drawing.Size(180, 22);
+            this.mitmLargeIcon.Text = "Large Icon";
+            // 
+            // mitmDetails
+            // 
+            this.mitmDetails.Name = "mitmDetails";
+            this.mitmDetails.Size = new System.Drawing.Size(180, 22);
+            this.mitmDetails.Text = "Details";
+            // 
+            // mitmSmallIcon
+            // 
+            this.mitmSmallIcon.Name = "mitmSmallIcon";
+            this.mitmSmallIcon.Size = new System.Drawing.Size(180, 22);
+            this.mitmSmallIcon.Text = "Small Icon";
+            // 
+            // mitmList
+            // 
+            this.mitmList.Name = "mitmList";
+            this.mitmList.Size = new System.Drawing.Size(180, 22);
+            this.mitmList.Text = "List";
+            // 
+            // mitmTile
+            // 
+            this.mitmTile.Name = "mitmTile";
+            this.mitmTile.Size = new System.Drawing.Size(180, 22);
+            this.mitmTile.Text = "Tile";
             // 
             // frmAracSorgulama
             // 
@@ -163,6 +234,7 @@
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
             this.Name = "frmAracSorgulama";
             this.Text = "frmAracSorgulama";
+            this.cmView.ResumeLayout(false);
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -183,5 +255,13 @@
         private System.Windows.Forms.ColumnHeader columnHeader4;
         private System.Windows.Forms.ColumnHeader columnHeader5;
         private System.Windows.Forms.ComboBox cbMarkalar;
+        private System.Windows.Forms.ContextMenuStrip cmView;
+        private System.Windows.Forms.ToolStripMenuItem mitmLargeIcon;
+        private System.Windows.Forms.ToolStripMenuItem mitmDetails;
+        private System.Windows.Forms.ToolStripMenuItem mitmSmallIcon;
+        private System.Windows.Forms.ToolStripMenuItem mitmList;
+        private System.Windows.Forms.ToolStripMenuItem mitmTile;
+        private System.Windows.Forms.ImageList ilLarge;
+        private System.Windows.Forms.ImageList ilSmall;
     }
 }
